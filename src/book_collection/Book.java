4dd732b85c64258ed5c2082cb684dbb2a2b4e308
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2026-03-04 21:41:56 -0800

    filled initial skeleton for book class

--- src/book_collection/Book.java
+++ src/book_collection/Book.java
@@ -1,5 +1,6 @@
 package book_collection;
 
+
 public class Book {
 	
 	// class fields
@@ -7,6 +8,11 @@ public class Book {
 	private String title;
 	private String author;
 	private int yearPublished;
+	
+	
+	// symbolic constants 
+	private static final int MIN_YEAR_PUBLISHED = 1450;
+	private static final int MAX_YEAR_PUBLISHED = 2026;
 	
 	
 	// constructor
@@ -17,23 +23,33 @@ public class Book {
 	public Book(final int id, String title, String author, int yearPublished) {
 		super();
 		this.id = id;
+		validateTitle(title);
 		this.title = title;
+		validateAuthor(author);
 		this.author = author;
+		yearPublished(yearPublished);
 		this.yearPublished = yearPublished;
 	}
 	
 	
 	// validations
-	private static void validateTitle() {
-		
+	private static void validateTitle(String title) {
+		if(title == null || title.isBlank()) {
+			throw new IllegalArgumentException("Error!..  Invalid title: " + title);
+		}
 	}
 	
-	private static void validateAuthor() {
-		
+	// would of just make validateString for title and author but need different error messages
+	private static void validateAuthor(String author) {
+		if(author == null || author.isBlank()) {
+			throw new IllegalArgumentException("Error!..  Invalid author: " + author);   
+		}
 	}
 	
-	private static void yearPublished() {
-		
+	private static void yearPublished(int year) {
+		if(year < MIN_YEAR_PUBLISHED || year > MAX_YEAR_PUBLISHED) {
+			throw new IllegalArgumentException("Error!..  Invalid year published: " + year);
+		}
 	}
 
 	
@@ -53,6 +69,13 @@ public class Book {
 
 	public int getYearPublished() {
 		return yearPublished;
+	}
+	
+	
+	
+	// class methods
+	public void getDescription() {
+		System.out.println("Book description here...");
 	}
 
 	
