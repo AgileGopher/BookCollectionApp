6934b75cf73fca47a888e6b2ff8ff004da90c1d0
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2026-03-15 23:26:05 -0700

    added some abstract method

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -30,6 +30,33 @@ public class Driver {
 	
 	private void runMainMenu() {
 		
+		int choice;
+		boolean proceed = true;
+		
+		String promptMsg = "Make a selection: \n";
+		String errorMsg  = "Invalid entry, enter an intger value in the range (1-7): \n";
+		
+		while(proceed) {
+			
+			System.out.println("------ Library Management System ------\n\n"
+			         + "-------------- Main Menu --------------\n"
+			         + "1. Add book\n"
+			         + "2. Add Audiobook\n"
+			         + "3. Search\n"
+			         + "4. Print Library\n"
+			         + "5. Save\n"
+			         + "6. Load\n"
+			         + "7. Exit\n");
+			
+			choice = kb.readInteger(promptMsg, errorMsg, 1, 7);
+			
+			switch(choice) {
+			
+				case 1:
+					
+				
+			}
+		}
 	}
 
 	
