4aa365131ccd2781bde954220a42ab5361dcc31d
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2026-04-10 21:22:34 -0700

    seting up new GUI

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+	
+
+	public static void main(String[] args) {
+		
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+
+	}
+
+	
+	
+	public MainFrame() {
+		
+	}
+	
+	
+}
