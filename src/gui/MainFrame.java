a6b69199538df0b4914e6e7a0fe5a863399366de
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2026-04-13 13:59:00 -0700

    added new component to catch null or blank input from user and return a joptionpane message stating error instead of crashing

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,9 +1,20 @@
 package gui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import data.Book;
+import data.LibraryItem;
+import management.LibraryManager;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
+	
+	LibraryManager manager = new LibraryManager();
 	
 
 	public static void main(String[] args) {
@@ -20,6 +31,33 @@ public class MainFrame extends JFrame {
 		setBounds(0, 0, 1450, 800);
 		setLocationRelativeTo(null);
 		setResizable(false);
+				
+		JPanel panel = new JPanel();
+		
+		JTextField titleField = new JTextField(20);
+		JButton addButton     = new JButton("Add Book");
+		JTextArea outputArea  = new JTextArea(10, 30);
+		
+		panel.add(titleField);
+		panel.add(addButton);
+		panel.add(outputArea);
+		
+		add(panel);
+		
+		addButton.addActionListener(e -> {
+			String title = titleField.getText();
+			if(title == null || title.isBlank()) {
+				JOptionPane.showMessageDialog(null, "Please choose!");
+			}
+			else {
+				Book book = new Book(1001, title, 2000, "Unknown");
+				manager.addItem(book);
+			}
+			outputArea.setText("");
+			for(LibraryItem item : manager.getItems()) {
+				outputArea.append(item.getDescription() + "\n");
+			}
+		});
 	}
 	
 	
